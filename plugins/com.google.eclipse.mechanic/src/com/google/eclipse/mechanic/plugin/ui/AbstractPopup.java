5fc98308afcc30bab7ec1380b91e67372c0c8ab0
Robert Konigsberg <konigsberg@gmail.com>
2011-02-21 22:58:08 -0700

    Clear up copyrights.

--- plugins/com.google.eclipse.mechanic/src/com/google/eclipse/mechanic/plugin/ui/AbstractPopup.java
+++ plugins/com.google.eclipse.mechanic/src/com/google/eclipse/mechanic/plugin/ui/AbstractPopup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2011, Google.
+ * Copyright (C) 2010, Google.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
