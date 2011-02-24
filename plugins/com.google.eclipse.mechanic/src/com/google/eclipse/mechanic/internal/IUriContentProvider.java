beaaafe34627a5ff4e60bc4f56b425328850664f
konigsberg@gmail.com <konigsberg@gmail.com>
2011-02-24 13:55:36 -0700

    IUrlContentProvider interface. This should have been committed a few changes back.

---
+++ plugins/com.google.eclipse.mechanic/src/com/google/eclipse/mechanic/internal/IUriContentProvider.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Google Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package com.google.eclipse.mechanic.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+/**
+ * Supplies InputStreams from URIs.
+ */
+public interface IUriContentProvider {
+  InputStream get(URI uri) throws IOException;
+  void clear();
+}
